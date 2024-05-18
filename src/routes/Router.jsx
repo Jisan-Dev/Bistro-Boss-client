aa12fb329fc7afffd3f06c4f2313ab0a8425d803
Jisun696 <istiakkashem35@gmail.com>
2024-05-18 23:38:43 +0600

    header component added

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/homepage/Home';
+import Root from './Root';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Root />,
   },
 ]);
 
