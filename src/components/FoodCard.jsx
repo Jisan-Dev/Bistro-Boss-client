5a8007f2e73f6460d24f6f872f0036544c1d87a1
Jisun696 <istiakkashem35@gmail.com>
2024-05-21 23:59:23 +0600

    addeed useayth custom hook

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,7 +1,15 @@
 /* eslint-disable react/prop-types */
 
+import useAuth from '../hooks/useAuth';
+
 const FoodCard = ({ item }) => {
+  const { user } = useAuth();
   const { name, image, price, recipe } = item;
+
+  const handleAddToCart = (item) => {
+    console.log(item);
+  };
+
   return (
     <div className="card w-96 bg-base-100 shadow-xl">
       <figure>
@@ -12,7 +20,11 @@ const FoodCard = ({ item }) => {
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-yellow-600 mt-4 hover:border-yellow-600 hover:text-yellow-600">Add to Cart</button>
+          <button
+            onClick={() => handleAddToCart(item)}
+            className="btn btn-outline bg-slate-100 border-0 border-b-4 border-yellow-600 mt-4 hover:border-yellow-600 hover:text-yellow-600">
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
