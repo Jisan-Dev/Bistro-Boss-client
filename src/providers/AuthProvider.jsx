509a899376e793de1ff2d168640025ef2dd3187d
Jisun696 <istiakkashem35@gmail.com>
2024-05-21 17:57:46 +0600

    Added auth context provider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react/prop-types */
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import { app } from '../firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        console.log('currentUser==> ', currentUser);
+      } else {
+        setUser(null);
+        console.log('currentUser==> ', currentUser);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = { user, loading, createUser, signIn };
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
