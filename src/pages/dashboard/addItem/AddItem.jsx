c06899357d28a842dcfccf585d63db7ddf213734
Jisun696 <istiakkashem35@gmail.com>
2024-05-24 22:11:54 +0600

    added functionalities to add a menu item

--- src/pages/dashboard/addItem/AddItem.jsx
+++ src/pages/dashboard/addItem/AddItem.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SectionHeader from '../../../components/SectionHeader';
 import { useForm } from 'react-hook-form';
 import { FaUtensils } from 'react-icons/fa';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const AddItem = () => {
   const { register, handleSubmit, reset } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const axiosPublic = useAxiosPublic();
+  const axiosSecure = useAxiosSecure();
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+    // image upload to imgbb & then get an url
+    const imageFile = { image: data.image[0] };
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        'content-Type': 'multipart/form-data',
+      },
+    });
+    console.log(res.data);
+    if (res.data.success) {
+      // now send the item data to the server with image display_url
+      const itemData = {
+        name: data.name,
+        category: data.category,
+        price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
+      };
+      const menuRes = await axiosSecure.post('/menu', itemData);
+      console.log(menuRes.data);
+      setLoading(false);
+      if (menuRes.data.insertedId) {
+        reset();
+        Swal.fire({
+          title: `<strong>${data.name}</strong> <br/> successfully added to the menu`,
+          icon: 'success',
+        });
+      }
+    }
+  };
 
   return (
     <div className="px-20">
@@ -54,8 +94,9 @@ const AddItem = () => {
 
           <div className="w-full my-6 flex justify-between">
             <input {...register('image', { required: true })} type="file" className="file-input w-full max-w-xs" />
+
             <button className="btn w-1/4 uppercase bg-orange-500 hover:bg-orange-600 font-bold text-base text-white">
-              Add Item <FaUtensils className="ml-4"></FaUtensils>
+              {loading ? 'Loading...' : 'Add Item'} <FaUtensils className="ml-4"></FaUtensils>
             </button>
           </div>
 
